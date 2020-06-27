eaefa2e480863edbf749797e63fb6c8c5f8e26e5
Samuel Larose <samlarose1997@hotmail.ca>
2020-06-27 16:59:55 -0400

    patch lib import now relative

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-import DateInput from '@/components/DateInput.vue'
-import DurationInput from '@/components/DurationInput.vue'
-import NumberInput from '@/components/NumberInput.vue'
-import RatingInput from '@/components/RatingInput.vue'
-import SwitchInput from '@/components/SwitchInput.vue'
-import TextAreaInput from '@/components/TextAreaInput.vue'
-import TextIconInput from '@/components/TextIconInput.vue'
-import TextInput from '@/components/TextInput.vue'
+import DateInput from './DateInput.vue'
+import DurationInput from './DurationInput.vue'
+import NumberInput from './NumberInput.vue'
+import RatingInput from './RatingInput.vue'
+import SwitchInput from './SwitchInput.vue'
+import TextAreaInput from './TextAreaInput.vue'
+import TextIconInput from './TextIconInput.vue'
+import TextInput from './TextInput.vue'
 
 const Components = {
     DateInput,
