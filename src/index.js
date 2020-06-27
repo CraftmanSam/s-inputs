d0dab877466cdc8cb3b16f618ac1ffd8cf2c2b7e
Samuel Larose <samlarose1997@hotmail.ca>
2020-06-27 16:40:18 -0400

    Add plugin

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import SInputs from '@/components'
+const version = '__VERSION__'
+
+const install = Vue => {
+  Object.keys(SInputs).forEach(name => {
+      Vue.component(name, SInputs[name])
+  })
+}
+
+const plugin = {
+  install,
+  version
+}
+
+export default plugin
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
